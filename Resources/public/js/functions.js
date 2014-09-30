b254ac6d30fec06547af8c661e13b57edbb49629
Phackwer <phackwer@gmail.com>
2014-09-30 17:50:09 -0300

    Ajustes para correto comportamento do errorPlacement

--- Resources/public/js/functions.js
+++ Resources/public/js/functions.js
@@ -137,22 +137,6 @@ function addNumber()
 	}
 }
 
-//function addMultiNumber()
-//{
-//	if ($(this).attr('name')) {
-//		$(this).attr('name', $(this).attr('name').replace( numberPattern , incrementNumber ))
-//	}
-//	if ($(this).prop('id')) {
-//		$(this).prop('id', $(this).prop('id').replace( numberPattern , incrementNumber ))
-//	}
-//	if ($(this).attr('targetId')) {
-//		$(this).attr('targetId', $(this).attr('targetId').replace( numberPattern , incrementNumber ))
-//	}
-//	if ($(this).attr('for')) {
-//		$(this).attr('for', $(this).attr('for').replace( numberPattern , incrementNumber ))
-//	}
-//}
-
 function reindexMultiNumberLabel(index)
 {
 	var numberPattern = /\d+/g;
@@ -228,8 +212,8 @@ function hideMinus(index)
 
 function hideMultiMinus(index)
 {
-	var visibleChildren = $(this).parent().parent().parent().find('ul:visible').length;
-	if (visibleChildren == 2) {
+	var visibleChildren = $(this).parent().parent().parent().find('ul:visible').filter('.clonable').length;
+	if (visibleChildren == 1) {
 		$(this).hide();
 	}
 	else {
@@ -337,74 +321,74 @@ function textAreaLimit(){
 }
 
 function errorPlacement (error, element) {
-//	unmask()
-	if (element.is(':checkbox')){
-		var parent = element.parent().parent();
-		var getSpanFrom = element.parent();
-	}
-	else{
-		var parent = element.parent();
-		var getSpanFrom = element;
-	}
-	
-	if(!parent.hasClass('validationSpan')){
-		var spanClass = getSpanFrom[0].outerHTML.match(/(span\d)/);
-		if (!spanClass)
-			var container = $('<span class="validationSpan"></span>');
-		else {
-			if (element.hasClass('dateBR')) {
-				value = parseInt(spanClass[0].substr(4)) + 1;
-				spanClass[0] = 'span'+ value;
-			}
-			var container = $('<span class="'+spanClass[0]+' validationSpan"></span>');
+	if (element) {
+		if (element.is(':checkbox')){
+			var parent = element.parent().parent();
+			var getSpanFrom = element.parent();
+		}
+		else{
+			var parent = element.parent();
+			var getSpanFrom = element;
 		}
 		
-		if (!parent.hasClass('nospaceuse')) {
-
-			if (element.is(':checkbox')) {
-				container.insertAfter($('label[for="'+element.prop('id')+'"]'));
-				container.append($('label[for="'+element.prop('id')+'"]'));
-			} else {
-				container.insertAfter(element)
-				container.append(element);
-			}
-			if (error) {
+		if(!parent.hasClass('validationSpan')){
+			var spanClass = getSpanFrom[0].outerHTML.match(/(span\d)/);
+			if (!spanClass)
+				var container = $('<span class="validationSpan"></span>');
+			else {
 				if (element.hasClass('dateBR')) {
-					var br = $('<br />').insertAfter(parent.find('.ui-datepicker-trigger'))
-					error.insertAfter(br);
+					value = parseInt(spanClass[0].substr(4)) + 1;
+					spanClass[0] = 'span'+ value;
 				}
-				else if (element.is(':checkbox')) {
-					error.insertAfter($('label[for="'+element.prop('id')+'"]'));
-					error.html('<span>&#8593;</span> Selecione pelo menos um item.');
+				var container = $('<span class="'+spanClass[0]+' validationSpan"></span>');
+			}
+			
+			if (!parent.hasClass('nospaceuse')) {
+	
+				if (element.is(':checkbox')) {
+					container.insertAfter($('label[for="'+element.prop('id')+'"]'));
+					container.append($('label[for="'+element.prop('id')+'"]'));
+				} else {
+					container.insertAfter(element)
+					container.append(element);
 				}
-				else {
-					error.insertAfter(element);
+				if (error) {
+					if (element.hasClass('dateBR')) {
+						var br = $('<br />').insertAfter(parent.find('.ui-datepicker-trigger'))
+						error.insertAfter(br);
+					}
+					else if (element.is(':checkbox')) {
+						error.insertAfter($('label[for="'+element.prop('id')+'"]'));
+						error.html('<span>&#8593;</span> Selecione pelo menos um item.');
+					}
+					else {
+						error.insertAfter(element);
+					}
+				}
+			} else {
+				var oldParent = parent;
+				container.insertAfter(oldParent)
+				container.append(oldParent);
+				if (error) {
+					error.insertAfter(oldParent);
 				}
 			}
-		} else {
-			var oldParent = parent;
-			container.insertAfter(oldParent)
-			container.append(oldParent);
-			if (error) {
-				error.insertAfter(oldParent);
+		}
+		else  if (error) {
+			if (element.hasClass('dateBR')) {
+				var br = $('<br />').insertAfter(parent.find('.ui-datepicker-trigger'))
+				error.insertAfter(br);
+			} 
+			else if (element.is(':checkbox')) {
+				error.insertAfter($('label[for="'+element.prop('id')+'"]'));
+				error.html('<span>&#8593;</span> Selecione pelo menos um item.');
+			}
+			else {
+				error.insertAfter(element);
 			}
 		}
+		$('textarea').each(textAreaLimit);
 	}
-	else  if (error) {
-		if (element.hasClass('dateBR')) {
-			var br = $('<br />').insertAfter(parent.find('.ui-datepicker-trigger'))
-			error.insertAfter(br);
-		} 
-		else if (element.is(':checkbox')) {
-			error.insertAfter($('label[for="'+element.prop('id')+'"]'));
-			error.html('<span>&#8593;</span> Selecione pelo menos um item.');
-		}
-		else {
-			error.insertAfter(element);
-		}
-	}
-	$('textarea').each(textAreaLimit);
-//	mask();
 }
 
 function findTabPane(element){
