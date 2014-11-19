f8453a18d03f4d6f4ddf18e369388db5e710885b
phackwer <phackwer@gmail.com>
2014-11-19 10:09:01 -0200

    Remoção de linha desnecessária que quebrava no Windows

--- Doctrine/DBAL/Types/AutoEncodeStringType.php
+++ Doctrine/DBAL/Types/AutoEncodeStringType.php
@@ -13,8 +13,7 @@ class AutoEncodeStringType extends StringType
     public function convertToDatabaseValue($value, AbstractPlatform $p)
     {
     	$dbchar =  \AppKernel::getInstance()->getContainer()->getParameter('database_charset'); 
-    	if ($dbchar == 'latin1') $dbchar = 'ISO-8859-1';
-        // convert from utf8 to latin1
+        // convert from db encoding to latin1
         return mb_convert_encoding($value, $dbchar, 'UTF-8');
     }
 
@@ -24,8 +23,7 @@ class AutoEncodeStringType extends StringType
     public function convertToPHPValue($value, AbstractPlatform $p)
     {
     	$dbchar =  \AppKernel::getInstance()->getContainer()->getParameter('database_charset'); 
-    	if ($dbchar == 'latin1') $dbchar = 'ISO-8859-1';
-        // convert from latin1 to utf8
+        // convert from db encoding to utf8
         return mb_convert_encoding($value, 'UTF-8', $dbchar);
     }
 }
