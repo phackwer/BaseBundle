b05ff29e9983174ad42d6b6e1c4e4ed84b4448ca
Pablo Santiago Sánchez <phackwer@gmail.com>
2015-03-02 17:59:22 -0300

    keep alive convertido em rota

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -31,7 +31,40 @@ class DefaultController extends BaseController
     {
         return $this->render($this->accountView, array(
             'entityData' => $this->getService()->getUserData(),
-            'formData'   => $this->getService()->getAccountFormData()
+            'formData' => $this->getService()->getAccountFormData(),
         ));
+    }
+
+    /**
+     * Mantém a sessão viva e também registra o usuário como on-line em algum serviço externo
+     *
+     * @return [type] [description]
+     */
+    public function keepAliveAction()
+    {
+        // Sessão de 60 minutos
+        if (!$this->get('session')->has("keepAlive") || $this->getRequest()->query->has('reset') || $this->getRequest()->request->has('reset')) {
+            $this->get('session')->set("keepAlive", time() + (3600));
+        }
+
+        //Tic tac - o tempo passando
+        $tempo = (int) $this->get('session')->get("keepAlive") - time();
+
+        //Usuário definido como on-line a cada minuto.
+        //Qualquer usuário com mais de dois minutos de lag será considerado offline
+        if (($tempo % 60) == 0) {
+
+        }
+
+        if ($tempo <= 0) {
+            //Define usuário como offline
+            //
+            //
+            //Destrói a sessão do usuário
+            $this->get('session')->destroy();
+            return $this->renderJson(array('hasSession' => false));
+        }
+
+        return $this->renderJson(array("keepAlive" => 1, "time" => $tempo));
     }
 }
